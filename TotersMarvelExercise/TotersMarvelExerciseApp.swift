64f7428542ec5b709635c9ee4165c986657ace45
Mohamad Mustapha <mohamadmustapha22@gmail.com>
2024-03-28 23:27:34 +0200

    Initial Commit

---
+++ TotersMarvelExercise/TotersMarvelExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  TotersMarvelExerciseApp.swift
+//  TotersMarvelExercise
+//
+//  Created by Mohamad Mustapha on 28/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct TotersMarvelExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
