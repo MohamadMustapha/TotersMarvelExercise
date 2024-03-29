fa319b1a9953e186cd1cd2d418e730fc04ab4ab0
Mohamad Mustapha <mohamadmustapha22@gmail.com>
2024-03-29 20:31:15 +0200

    👷🏻 chore: add modularization architecture for Home and Details, add KingFisher and Swiftlint

---
+++ Details/Package.swift
@@ -0,0 +1,82 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package: Package = .init(
+    name: .details,
+    platforms: [.iOS],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .detailsProduct
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        .kingFisherPackageDependency,
+        .swiftLintPackageDependency
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .detailsTarget,
+        .detailsTestTarget
+    ]
+)
+
+fileprivate extension Product {
+
+    static let detailsProduct: Product = .library(name: .details,
+                                                  targets: [.details])
+}
+
+fileprivate extension Package.Dependency {
+
+    static let kingFisherPackageDependency: Package.Dependency = package(url: "https://github.com/onevcat/Kingfisher.git",
+                                                                         exact: "7.10.2")
+
+    static let swiftLintPackageDependency: Package.Dependency = package(url: "https://github.com/realm/SwiftLint",
+                                                                        exact: "0.54.0")
+}
+
+fileprivate extension String {
+
+    // MARK: Folders
+    static let details: String = "Details"
+
+    // MARK: Packages
+    static let kingFisher: String = "Kingfisher"
+    static let swiftLint: String = "SwiftLint"
+
+    // MARK: Plugins
+    static let swiftLintPlugin: String = "SwiftLintPlugin"
+
+    var testTarget: String { "\(self)Tests" }
+}
+
+fileprivate extension SupportedPlatform {
+
+    static let iOS: SupportedPlatform = .iOS(.v15)
+}
+
+fileprivate extension Target {
+
+    static let detailsTarget: Target = target(name: .details,
+                                              dependencies: [.kingFisherDependency],
+                                              plugins: [.swiftLintPlugin])
+    static let detailsTestTarget: Target = testTarget(name: .details.testTarget,
+                                                      dependencies: [.detailsDependency],
+                                                      plugins: [.swiftLintPlugin])
+}
+
+fileprivate extension Target.Dependency {
+
+    static let detailsDependency: Target.Dependency = byName(name: .details)
+    static let kingFisherDependency: Target.Dependency = byName(name: .kingFisher)
+}
+
+fileprivate extension Target.PluginUsage {
+
+    static let swiftLintPlugin: Target.PluginUsage = plugin(name: .swiftLintPlugin,
+                                                            package: .swiftLint)
+}
+
---
+++ Home/Package.swift
@@ -0,0 +1,82 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package: Package = .init(
+    name: .home,
+    platforms: [.iOS],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .homeProduct
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        .kingFisherPackageDependency,
+        .swiftLintPackageDependency
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .homeTarget,
+        .homeTestTarget
+    ]
+)
+
+fileprivate extension Product {
+
+    static let homeProduct: Product = .library(name: .home,
+                                               targets: [.home])
+}
+
+fileprivate extension Package.Dependency {
+
+    static let kingFisherPackageDependency: Package.Dependency = package(url: "https://github.com/onevcat/Kingfisher.git",
+                                                                         exact: "7.10.2")
+
+    static let swiftLintPackageDependency: Package.Dependency = package(url: "https://github.com/realm/SwiftLint",
+                                                                        exact: "0.54.0")
+}
+
+fileprivate extension String {
+
+    // MARK: Folders
+    static let home: String = "Home"
+
+    // MARK: Packages
+    static let kingFisher: String = "Kingfisher"
+    static let swiftLint: String = "SwiftLint"
+
+    // MARK: Plugins
+    static let swiftLintPlugin: String = "SwiftLintPlugin"
+
+    var testTarget: String { "\(self)Tests" }
+}
+
+fileprivate extension SupportedPlatform {
+
+    static let iOS: SupportedPlatform = .iOS(.v15)
+}
+
+fileprivate extension Target {
+
+    static let homeTarget: Target = target(name: .home,
+                                           dependencies: [.kingFisherDependency],
+                                           plugins: [.swiftLintPlugin])
+    static let homeTestTarget: Target = testTarget(name: .home.testTarget,
+                                                   dependencies: [.homeDependency],
+                                                   plugins: [.swiftLintPlugin])
+}
+
+fileprivate extension Target.Dependency {
+
+    static let homeDependency: Target.Dependency = byName(name: .home)
+    static let kingFisherDependency: Target.Dependency = byName(name: .kingFisher)
+}
+
+fileprivate extension Target.PluginUsage {
+
+    static let swiftLintPlugin: Target.PluginUsage = plugin(name: .swiftLintPlugin,
+                                                            package: .swiftLint)
+}
+
